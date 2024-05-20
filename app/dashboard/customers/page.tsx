4cf8dd6215056831cb70ae04560e6be2a06a79d1
Pankaj Kumar <pkmahan786@gmail.com>
2024-05-20 09:19:08 +0530

    implement customer page using suspense

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const query = searchParams?.query || '';
+  return (
+    <Suspense>
+      <CustomersTable query={query} />
+    </Suspense>
+  );
 }
